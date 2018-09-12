4f704984d983137153de3a9a522a405c1b7978c9
Daniel Limanowski <dlimanow@iastate.edu>
2018-09-12 10:56:49 -0500

    push before leaving lab

--- shell.c
+++ shell.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <unistd.h> // for getopt
 
 int main(int argc, char **argv) {
-    int i = 0;
-    for (i = 0; i < argc; i++) {
-        printf("Option %d is \"%s\"\n", i, argv[i]);
+    // Get the custom prompt, if there is one:
+    char prompt[25];
+
+    if (argc > 1) {
+        if (argv[1] == "-p") {
+            strcpy(prompt, argv[2])
+        }
+        strcpy(prompt, argv[1]);
+    } else {
+        strcpy(prompt, "308sh> ");
     }
+
+    printf("Prompt is: %s", prompt);
     return 0;
 }
