89c44a8f038ccbc11399bff632b69bf4cb44500c
Daniel Limanowski <dlimanow@iastate.edu>
2018-09-25 22:36:10 -0500

    prompt works

--- shell.c
+++ shell.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h> // for getopt
+#include <unistd.h>
+#include <stdlib.h>
+#define PROMPT_SIZE 25
 
 int main(int argc, char **argv) {
-    // Get the custom prompt, if there is one:
-    char prompt[25];
+        // Get the custom prompt, if there is one:
+        char prompt[PROMPT_SIZE];
 
-    if (argc > 1) {
-        if (argv[1] == "-p") {
-            strcpy(prompt, argv[2])
+        if (argc > 1) {
+                if (strcmp(argv[1], "-p") == 0) {
+                        strncpy(prompt, argv[2], PROMPT_SIZE);
+                } else {
+                        printf("USAGE: ./shell {-p %cdesired prompt%c}"
+                               " {-h/-help}\n", '"', '"');
+                        exit(EXIT_FAILURE);
+                }
+        } else {
+                strcpy(prompt, "308sh> ");
         }
-        strcpy(prompt, argv[1]);
-    } else {
-        strcpy(prompt, "308sh> ");
-    }
 
-    printf("Prompt is: %s", prompt);
-    return 0;
+        printf("Prompt is: %s\n", prompt);
+        return 0;
 }
